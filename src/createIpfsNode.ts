ecaaa14e92861a02d8e1f46ac9aff2981b587317
Jure Bogunovic <bogunovij@gmail.com>
2021-12-30 00:14:23 +0100

    no longer listing pins on startup

--- src/createIpfsNode.ts
+++ src/createIpfsNode.ts
@@ -8,10 +8,5 @@ export const createIpfsNode = async (): Promise<IPFS.IPFS> => {
   console.log("IPFS ID", await ipfsNode.id());
   console.log("isOnline", await ipfsNode.isOnline());
 
-  console.log("Listing pins...");
-  for await (const { cid } of await ipfsNode.pin.ls()) {
-    console.log(cid);
-  }
-
   return ipfsNode;
 };
